1e7f25fa170fa3f550d133dd27c87f15bd553d20
mgmarlow <mgmarlow@gmail.com>
2020-02-21 14:35:43 -0800

    Remove capybara rails_helpers

--- test-app/spec/rails_helper.rb
+++ test-app/spec/rails_helper.rb
@@ -4,9 +4,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
-if Rails.env.production?
-  abort("The Rails environment is running in production mode!")
-end
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "spec_helper"
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -54,8 +52,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  Capybara.javascript_driver = :webkit
 end
-
-Capybara::Webkit.configure(&:block_unknown_urls)
